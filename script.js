54a63246d8637b6a9f9fdb95f6073d27ff546d93
Silje Sørland <72661374+siljesorland@users.noreply.github.com>
2020-12-05 14:58:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+question 1
+var firstvar;
+
+question 2
+var name = "silje";
+
+question 3
+var integer = 4;
+
+question 4
+var division = 20 / 5;
+
+question 5
+var animal = "frog";
+animal === 'frog';
+
+question 6
+var orderHasBeenShipped; = "True";
+
+question 7
+if(answer === true) {
+    console.log("True");
+}
+else {
+    console.log("False");
+}
+
+question 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
